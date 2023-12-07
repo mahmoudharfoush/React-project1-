3b03a63d4dfb6114eb1d2d49900a6cf5ff1199f5
mahmoudharfoush <mahmoudharfoush5320@gmail.com>
2023-12-07 14:58:30 +0200

    first commit

---
+++ src/validation/uservalidation.js
@@ -0,0 +1,21 @@
+export const validationUserData = (user)=>{
+
+    let errors = {};
+
+    if(user.name.trim()==""){
+        errors.name="username is required"; 
+    }else if(user.name.trim().length <3){
+        errors.name="username must be at least 3 characters ";
+    }
+    if(user.email.trim()==""){
+        errors.email="useremail is required"; 
+    }else if(user.email.trim().length <3){
+        errors.email="useremail must be at least 3 characters ";
+    }
+    if(user.password.trim()==""){
+        errors.password="password is required"; 
+    }else if(user.password.trim().length <3){
+        errors.password="password must be at least 3 characters ";
+    }
+    return errors;
+}
